4ac84adad4c5ad0e4add5f21783bba6dbcdbeb68
hasbi.tlm <hasbi@theloudminority.co.id>
2019-03-29 05:14:09 +0700

    Fis: Student Job Interest

--- src/router/studentJobInterestRouter.js
+++ src/router/studentJobInterestRouter.js
@@ -1,5 +1,6 @@
 import { checkBody } from '../lib/validator';
 import express from 'express';
+import sequelize from '../database/sequelize';
 import {
   errorResponse,
   jwtAuth,
@@ -26,9 +27,7 @@ router.get('/', pagingParams, (req, res) => {
     where: whereClause,
     offset,
     limit,
-    include: [
-      { model: JobRole, as: 'jobRole', attributes: [ 'id', 'jobRole' ]}
-    ],
+    include: [{ model: JobRole, as: 'jobRole', attributes: [ 'id', 'jobRole' ]}],
     order: [[ 'highlight', 'DESC' ]]
   }).
     then(result => {
@@ -62,18 +61,10 @@ router.get('/:id', (req, res) => {
 
 router.post(
   '/',
-  checkBody([
-    { field: 'studentResumeId' },
-    { field: 'jobRoleId' }
-  ]),
+  checkBody([{ field: 'studentResumeId' }, { field: 'jobRoleId' }]),
   (req, res) => {
     try {
-      const {
-        studentResumeId,
-        jobRoleId,
-        experience,
-        highlight
-      } = req.body;
+      const { studentResumeId, jobRoleId, experience, highlight } = req.body;
 
       StudentJobInterest.create({
         studentResumeId,
@@ -100,43 +91,92 @@ router.post(
 );
 
 router.put('/:id', (req, res) => {
-  StudentJobInterest.findByPk(req.params.id).
-    then(obj => {
-      if (!obj) {
-        return res.json({
-          status: responseStatus.NOT_FOUND,
-          message: 'Data not found !'
-        });
-      }
-      const {
-        jobRoleId,
-        experience,
-        highlight
-      } = req.body;
+  if (req.params.id === 'bulk') {
+    const data = req.body;
 
-      obj.
-        update({
-          jobRoleId: jobRoleId || obj.jobRoleId,
-          experience: experience || obj.experience,
-          highlight: highlight === 'undefined' ?
-            obj.highlight :
-            highlight
-        }).
-        then(() =>
-          res.json({
-            status: responseStatus.SUCCESS,
-            message: 'Data updated !',
-            result: {
-              id: obj.id
+    if (Array.isArray(data)) {
+      sequelize.
+        transaction(tr => {
+          const processList = [];
+
+          for (const dt of data) {
+            let updatedData = {};
+
+            if (dt.jobRoleId) {
+              updatedData = {
+                updatedData,
+                ...{ jobRoleId: dt.jobRoleId }
+              };
             }
-          })).
+            if (dt.experience) {
+              updatedData = {
+                updatedData,
+                ...{ experience: dt.experience }
+              };
+            }
+            if (dt.highlight !== 'undefined') {
+              updatedData = {
+                updatedData,
+                ...{ highlight: dt.highlight }
+              };
+            }
+
+            processList.push(
+              StudentJobInterest.update(updatedData, {
+                where: { id: dt.id },
+                transaction: tr
+              })
+            );
+          }
+
+          return Promise.all(processList);
+        }).
+        then(() => {
+          return res.json({
+            message: 'Data Saved !',
+            result: {
+              status: responseStatus.SUCCESS,
+              message: 'Data updated !'
+            }
+          });
+        }).
         catch(error => {
           return errorResponse(error, res);
         });
-    }).
-    catch(error => {
-      return errorResponse(error, res);
-    });
+    }
+  } else {
+    StudentJobInterest.findByPk(req.params.id).
+      then(obj => {
+        if (!obj) {
+          return res.json({
+            status: responseStatus.NOT_FOUND,
+            message: 'Data not found !'
+          });
+        }
+        const { jobRoleId, experience, highlight } = req.body;
+
+        obj.
+          update({
+            jobRoleId: jobRoleId || obj.jobRoleId,
+            experience: experience || obj.experience,
+            highlight: highlight === 'undefined' ? obj.highlight : highlight
+          }).
+          then(() =>
+            res.json({
+              status: responseStatus.SUCCESS,
+              message: 'Data updated !',
+              result: {
+                id: obj.id
+              }
+            })).
+          catch(error => {
+            return errorResponse(error, res);
+          });
+      }).
+      catch(error => {
+        return errorResponse(error, res);
+      });
+  }
 });
 
 router.delete('/:id', (req, res) => {
